2a7e7e364c9f91500978604bd2528dc6dc8e7b23
ksdperera <sajith4012>
2016-02-18 16:12:51 +0530

    Adding Log Agent Main

---
+++ modules/components/org.wso2.carbon.la.log.agent/src/main/java/org/wso2/carbon/la/log/agent/AgentMain.java
@@ -0,0 +1,42 @@
+package org.wso2.carbon.la.log.agent;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointAgentConfigurationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointAuthenticationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointConfigurationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointException;
+import org.wso2.carbon.databridge.commons.exception.TransportException;
+import org.wso2.carbon.la.log.agent.agentConf.ConfigLogAgent;
+import org.wso2.carbon.la.log.agent.data.LogPublisher;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by sajithd on 2/18/16.
+ */
+public class AgentMain {
+    public static void main(String[] args) throws FileNotFoundException {
+        Gson gson = new Gson();
+        LogPublisher logPublisher;
+        JsonReader jsonReader = new JsonReader(new FileReader("/home/sajithd/WSO2_LogAnalyzer/product-la/modules/components/org.wso2.carbon.la.log.agent/src/main/java/org/wso2/carbon/la/log/agent/util/sample.json"));
+
+        ConfigLogAgent configLogAgent = gson.fromJson(jsonReader , ConfigLogAgent.class);
+        try {
+            logPublisher = new LogPublisher(configLogAgent.getOutput().getLoganalyzer());
+        } catch (DataEndpointAuthenticationException e) {
+            e.printStackTrace();
+        } catch (DataEndpointAgentConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransportException e) {
+            e.printStackTrace();
+        } catch (DataEndpointException e) {
+            e.printStackTrace();
+        } catch (DataEndpointConfigurationException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
